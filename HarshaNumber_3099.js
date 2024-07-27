9781c820d51001fd76011780a948e923e525fe1e
Maaz Ai-enthusiast <maaz.ai.enthusiast@gmail.com>
2024-07-27 10:15:25 -0700

    Differ b/w toSring and x.toString()

--- HarshaNumber_3099.js
+++ HarshaNumber_3099.js
@@ -1,3 +1,17 @@
+/*
+
+Availability: x.toString() is available on all objects that inherit from Object.prototype.
+ toString(x) would only work if you defined a global or local function named toString.
+
+Usage Context: x.toString() is used as a method of an object,
+ whereas toString(x) would be a standalone function call, which doesn’t exist in standard JavaScript.
+
+Type Coercion: To convert a value to a string, you can use String(x)
+ or x.toString(), but toString(x) will result in a reference error unless you define such a function.
+
+*/ 
+
+
 var sumOfTheDigitsOfHarshadNumber = function(x) {
     let str = x.toString(); 
     let arr = [...str];
