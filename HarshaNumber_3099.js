a7c429314206e30069a8b6dd3322b6ff9ab1a376
Maaz Ai-enthusiast <maaz.ai.enthusiast@gmail.com>
2024-07-27 10:09:57 -0700

    Leetcode: 3099

--- HarshaNumber_3099.js
+++ HarshaNumber_3099.js
@@ -1,16 +1,13 @@
-var sumOfTheDigitsOfHarshadNumber = function (x) {
-    let str = toString(x);
-    let arr = [...str]
+var sumOfTheDigitsOfHarshadNumber = function(x) {
+    let str = x.toString(); 
+    let arr = [...str];
     let sum = 0;
     for (const n of arr) {
         sum += parseInt(n);
     }
     if (x % sum === 0) {
         return sum;
-    }
-    else {
+    } else {
         return -1;
     }
-
 };
-console.log(sumOfTheDigitsOfHarshadNumber(18));
