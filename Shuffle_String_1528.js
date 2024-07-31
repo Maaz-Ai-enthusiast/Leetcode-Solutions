700d8e441e54c399ed0ca43bf8a557bbd5d5b0f6
Maaz Ai-enthusiast <maaz.ai.enthusiast@gmail.com>
2024-07-31 08:44:02 -0700

    learn new Array

---
+++ Shuffle_String_1528.js
@@ -0,0 +1,17 @@
+// var restoreString = function(s, indices) {
+//     let shuffleStr=[];
+//     for(let i=0;i<s.length;i++){
+//        let curVal=indices[i];
+//        shuffleStr.push(s[curVal]);
+//     }
+//     return shuffleStr.join("")
+// };
+
+var restoreString = function(s, indices) {
+    let shuffleStr = new Array(s.length);
+    for (let i = 0; i < s.length; i++) {
+        shuffleStr[indices[i]] = s[i];
+    }
+    return shuffleStr.join("");
+};
+console.log(restoreString( "codeleet",[4,5,6,7,0,2,1,3]));
